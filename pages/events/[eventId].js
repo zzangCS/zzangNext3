664787cd8282baa56de224d4e10504450b938beb
skyhanull <anky0425@naver.com>
2023-02-14 19:00:04 +0900

    feat:동적세그먼트추가

---
+++ pages/events/[eventId].js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  console.log(eventId);
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>no event found</p>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.address}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+};
+
+export default EventDetailPage;
