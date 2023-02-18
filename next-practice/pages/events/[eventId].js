805c6e9ba31b243feaf9cca2fe269f4ed70674f0
skyhanull <anky0425@naver.com>
2023-02-18 13:26:13 +0900

    feat: 동적페이지를 props와 paths로 사전페이지 구현

--- next-practice/pages/events/[eventId].js
+++ next-practice/pages/events/[eventId].js
@@ -1,15 +1,10 @@
-import { useRouter } from "next/router";
-import { getEventById } from "../../dummy-data";
 import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
-const EventDetailPage = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  console.log(eventId);
-  const event = getEventById(eventId);
+import { getEventById, getAllEvents } from "../../helpers/api-utill";
+const EventDetailPage = (props) => {
+  const event = props.selectedEvent;
 
   if (!event) {
     return <p>no event found</p>;
@@ -29,5 +24,27 @@ const EventDetailPage = () => {
     </Fragment>
   );
 };
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((el) => ({ params: { eventId: el.id } }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
 
 export default EventDetailPage;
