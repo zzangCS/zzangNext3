45279a57ec9fe396bebe2c70fc4ddb031ade4cdb
skyhanull <anky0425@naver.com>
2023-02-18 13:25:24 +0900

    add: getStaticProps추가

--- next-practice/pages/index.js
+++ next-practice/pages/index.js
@@ -1,12 +1,23 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-utill";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
+  const { featuredEvents } = props;
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.featuredEvents} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
